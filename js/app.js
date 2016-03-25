7d4a14c17929ca7358fc223a4b01703bc4f3ea00
meep meep me <ianktalbert2me.com>
2016-03-25 11:23:33 -1000

    finish :3
    added auto clicking workers :3333

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var log = 0;
 var money = 0;
 var upgrade = 1;
 var upgradecost = 1;
+var worker = 0;
+var workercost = 1;
 function addlog() {
 	 log = log + upgrade;
 	console.log('logs' + log);
@@ -49,3 +51,29 @@ function upgradelog() {
 		console.log(money + 'is not enough');
 	};
 }
+function autoclicker(){
+	if (money >= 600 * workercost) {
+		workercost = workercost * 1.5;
+		money = money-600*workercost;
+		worker++;
+		document.getElementById('workercost').innerHTML = workercost*600;
+		document.getElementById('money').innerHTML = money;
+	}else
+		alert('You so not have enough money to buy a worker')
+}
+var workerwork = function(){
+	setInterval(function(){
+		log = log+(worker*5);
+		document.getElementById('log').innerHTML = log;
+	}, 1000)
+}
+
+//audio
+var boom = new Audio("music/TCTM.mp3");
+var countUp = function (){
+	var condition = true;
+	boom.loop = condition;
+	boom.play()
+}
+countUp();
+//end of audio
